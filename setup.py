f4eeb3cd8367a2dfc2ec22ea36366dac05ccb4cc
bnsantoso <137144235+bnsantoso@users.noreply.github.com>
2023-07-18 02:55:31 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
-with open("README.md", "r") as README:
-    long_description = README.read()
+with open("README.md", "r", encoding="utf-8") as readme_file:
+    README = readme_file.read()
 
 setup(
     name="subtoaudio",
@@ -9,13 +9,13 @@ setup(
     version="0.1",
     license="MPL 2.0",
     description="Generate audio or speech from any subtitle file",
-    author="Bagas Nugroho Santoso",
+    author="Bagas NS",
     author_email="bagassantoso71@gmail.com",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/bnsantoso/",
-    install_requires=["TTS","pydub","librosa"],
-    keywords=["subtitle", "tts", "text to audio", "subtitle to audio", "subtitle to speech"],
+    install_requires=["TTS","pydub","librosa",],
+    keywords=["subtitle", "tts", "text to audio", "subtitle to audio", "subtitle to speech",],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
